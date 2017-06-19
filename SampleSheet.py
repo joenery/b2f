0370cb31694f7aeede753f9adbd7cbf82b8fc6ed
joenery <jnery@salk.edu>
2017-06-19 13:45:53 -0700

    samplehseet.py line_with

--- SampleSheet.py
+++ SampleSheet.py
@@ -1,7 +1,7 @@
 import pandas
 import os
 
-def find_line_with(text, in_file):
+def line_with(text, in_file):
     """ Find first occurrence of a string in the given file
 
     Returns the line number of the string
