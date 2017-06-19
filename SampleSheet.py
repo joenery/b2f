11e5dfe76dc83c84b5ede45b192568e9c1325f33
joenery <jnery@salk.edu>
2017-06-19 13:54:31 -0700

    samplehseet.py

--- SampleSheet.py
+++ SampleSheet.py
@@ -6,4 +6,7 @@ def line_with(text, in_file):
 
     Returns the line number of the string
     """
-
+    with open(in_file) as fh:
+        for i, line in enumerate(fh, 1):
+            if text in line:
+                return i
