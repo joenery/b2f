4f944f8662935ae862ffb12cfef950034920d606
joenery <jnery@salk.edu>
2017-06-19 16:41:00 -0700

    samplesheet

--- SampleSheet.py
+++ SampleSheet.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import os
 
+samplesheet = "SampleSheet_170612_K00161_0142.csv"
+
 def line_with(text, in_file):
     """ Get the line number of the given text string in a file
 
@@ -11,12 +13,16 @@ def line_with(text, in_file):
             if text in line:
                 return i
 
-def read_sample_sheet(header_line, in_file):
+def read_sample_sheet(in_file):
     """ Read a SampleSheet.csv file into a Pandas data frame
 
     Returns the data frame
     """
-    return pd.read_csv(in_file, header=header_line)
+    return pd.read_csv(in_file, header = line_with("[Data]", in_file))
     
+sheet = read_sample_sheet(os.path.abspath(samplesheet))
 
+samples = list(sheet.Sample_ID)
 
+for sample in samples:
+    print sample
